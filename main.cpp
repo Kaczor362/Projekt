b06b8a2b51131463d9cb1960514730347458f6f1
Kaczor <kaczorowski0000@gmail.com>
2024-06-18 19:43:12 +0200

    chaning names of functions

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <string.h>
 
 int main() {
-    FILE *fp;
+    FILE *plik;
     char filename[100];
     char text[256]; //maksymalna wielkość chara to 256 bitów
     int choice;
@@ -21,17 +21,17 @@ int main() {
             fgets(filename, sizeof(filename), stdin);
             filename[strcspn(filename, "\n")] = 0; // Usunięcie znaku nowej linii
 
-            fp = fopen(filename, "w");
-            if (fp == NULL) {
+            plik = fopen(filename, "w");
+            if (plik == NULL) {
                 perror("Error while trying to save");
                 continue; // Powrót do menu
             }
 
             printf("Enter text (end with empty line):\n");
-            while (fgets(text, sizeof(text), stdin) != NULL && text[0] != '\n') {
-                fputs(text, fp);
+            while (fgets(text, sizeof(text), stdin) != NULL && text[0] != '\n') { //stdin odczyt z klawiatury 'n' to sprawdzenie czy nie jest to nowa linia (jesli tak to petla sie konczy)
+                fputs(text, plik); //zapisz tekst do pliku plik
             }
-            fclose(fp);
+            fclose(plik);
             printf("Saved to file: %s\n", filename);
 
         } else if (choice == 2) {
@@ -39,17 +39,17 @@ int main() {
             fgets(filename, sizeof(filename), stdin);
             filename[strcspn(filename, "\n")] = 0; // Usunięcie znaku nowej linii
 
-            fp = fopen(filename, "r");
-            if (fp == NULL) {
+            plik = fopen(filename, "r");
+            if (plik == NULL) {
                 perror("No such file.");
                 continue; // Powrót do menu
             }
 
             printf("\nContent:\n");
-            while (fgets(text, sizeof(text), fp) != NULL) {
+            while (fgets(text, sizeof(text), plik) != NULL) {
                 printf("%s", text);
             }
-            fclose(fp);
+            fclose(plik);
 
         } else if (choice == 3) {
             break; // Zakończenie programu
